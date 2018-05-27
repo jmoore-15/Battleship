c63a5c3dcc320014039860a57775f2a3f9943bfe
Justin Moore <jmoo@Justins-MacBook-Pro.local>
2018-05-27 15:39:30 -0400

    Almost done with randomly generating a computers board placements

--- board.h
+++ board.h
@@ -11,122 +11,155 @@ class board {
             for(int i = 0; i < 10; i++) {
                 for(int j = 0; j < 10; j++) {
                     boardSlot slot;
-                    slot.yAxis = i;
-                    slot.xAxis = j;
+                    slot.xAxis = i;
+                    slot.yAxis = j;
                     slot.coordinate = to_string(slot.xAxis) + to_string(slot.yAxis);
 
-                    gameMatrix[i][j] = boardSlot();
+                    gameMatrix[i][j] = slot;
                 }
             }
         }
 
         /******** Each slot in the board ********/
-        class boardSlot {
-            public:
+        struct boardSlot {
             boardSlot() {
-                    hit = false;
-                }
-            private:
-                bool taken;        // Is there a ship there
-                bool hit;          // If there's a ship there, was it hit?
-                int xAxis;         // e.g. 5
-                int yAxis;         // e.g. 3 would be C
-                string coordinate; // e.g. 5C
+                taken = false;
+                hit = false;
+                xAxis = -1;
+                yAxis = -1;
+                coordinate = "";
+                shipName = "";
+            }
+            bool taken;        // Is there a ship there
+            bool hit;          // If there's a ship there, was it hit?
+            int xAxis;         // e.g. 5
+            int yAxis;         // e.g. 2 would be C
+            string coordinate; // e.g. 52
+            string shipName;   // Destroyer
         };
 
         /******** Each boat on the board ********/
-        class ship {
-            public:
-                ship(int s) {
-                    size = s;
-                    sunk = false;
-                }
+        struct ship {
+            ship(string n, int s) {
+                name = n;
+                size = s;
+                sunk = false;
+            }
+
+            map<string, bool> locations; // e.g. 5C, has it been hit?
+            bool sunk;                   // Is this ship sunk?
+            int size;                    //    5   ,     4     ,    3   ,     3    ,     2
+            string name;                 // Carrier, Battleship, Cruiser, Submarine, Destroyer
+
             /* Pick computer ship location */
-            bool placeShip() {
-                int xAxisChoice = rand() % 9;
-                int yAxisChoice = rand() % 9;
+            bool placeShip(board gameBoard) {
+                while(true) {            // Until every spot in the ship has been placed on the board
 
-                if(!gameMatrix[xAxisChoice][yAxisChoice].taken) { // If the board slot isn't taken
+                    int xAxisChoice = rand() % 9;
+                    int yAxisChoice = rand() % 9;
 
-                    if(canBeHorizontal(xAxisChoice, yAxisChoice, size) || canBeVertical(xAxisChoice, yAxisChoice, size)) {
-                        bool isItHorizontal = rand() % 2 == 1;    // Should the chip be horizontal (1) or vertical (0)
+                    if(!gameBoard.gameMatrix[xAxisChoice][yAxisChoice].taken) {          // If the board slot isn't taken
 
-                        if(isItHorizontal && canBeHorizontal(xAxisChoice, yAxisChoice, size)) {
-                            if(canBeHorizontalLeft()) {
-                                for(int i = 0; i < size; i++) {
-                                    
+                        if(canBeHorizontal(xAxisChoice, yAxisChoice, gameBoard, size) || // Can it be placed somewhere on that coord?
+                           canBeVertical(xAxisChoice, yAxisChoice, gameBoard, size))
+                        {
+                            bool isItHorizontal = rand() % 2 == 1;             // Should the ship be horizontal (1) or vertical (0)
+
+                            if(isItHorizontal && canBeHorizontal(xAxisChoice, yAxisChoice, gameBoard, size)) {    // It can be horizontal
+
+                                bool isItHorizontalLeft = rand() % 2 == 1;     // Should the ship be horizontalLeft (1) or right (0)
+
+                                if(isItHorizontalLeft && canBeHorizontalLeft(xAxisChoice, yAxisChoice, gameBoard, size)) {
+                                    for(int i = 0; i < size; i++) {
+                                        gameBoard.gameMatrix[xAxisChoice - i][yAxisChoice].taken = true;          // Game slot is now used
+                                        gameBoard.gameMatrix[xAxisChoice - i][yAxisChoice].shipName = this->name; // Type of ship
+                                        string coordinate = to_string(xAxisChoice - i) + to_string(yAxisChoice);  // Coordinate
+                                        this->locations.insert(pair<string, bool>(coordinate, false));            // Ship now has coord
+                                    }
+                                }
+                                else {
+                                    for(int i = 0; i < size; i++) {
+                                        gameBoard.gameMatrix[xAxisChoice + i][yAxisChoice].taken = true;          // Game slot is now used
+                                        gameBoard.gameMatrix[xAxisChoice + i][yAxisChoice].shipName = this->name; // Type of ship
+                                        string coordinate = to_string(xAxisChoice + i) + to_string(yAxisChoice);  // Coordinate
+                                        this->locations.insert(pair<string, bool>(coordinate, false));            // Ship now has coord
+                                    }
                                 }
                             }
-                            else {
+                            else if(!isItHorizontal && canBeVertical(xAxisChoice, yAxisChoice, gameBoard, size)) {// It can be vertical
 
-                            }
-                        }
-                        else {
-                            if(canBeVerticalAbove()) {
+                                bool isItVerticalAbove = rand() % 2 == 1;     // Should the ship be verticalAbove (1) or below (0)
 
+                                if(isItVerticalAbove && canBeVerticalAbove(xAxisChoice, yAxisChoice, gameBoard, size)) {
+                                    for(int i = 0; i < size; i++) {
+                                        gameBoard.gameMatrix[xAxisChoice][yAxisChoice - i].taken = true;          // Game slot is now used
+                                        gameBoard.gameMatrix[xAxisChoice][yAxisChoice - i].shipName = this->name; // Type of ship
+                                        string coordinate = to_string(xAxisChoice) + to_string(yAxisChoice - i);  // Coordinate
+                                        this->locations.insert(pair<string, bool>(coordinate, false));            // Ship now has coord
+                                    }
+                                }
+                                else {
+                                    for(int i = 0; i < size; i++) {
+                                        gameBoard.gameMatrix[xAxisChoice][yAxisChoice + i].taken = true;          // Game slot is now used
+                                        gameBoard.gameMatrix[xAxisChoice][yAxisChoice + i].shipName = this->name; // Type of ship
+                                        string coordinate = to_string(xAxisChoice) + to_string(yAxisChoice + i);  // Coordinate
+                                        this->locations.insert(pair<string, bool>(coordinate, false));            // Ship now has coord
+                                    }
+                                }
                             }
-                            else {
-
-                            }
+                            return true;
                         }
                     }
                 }
-                return true;
             }
 
             /* Can the ship be placed horizontally? */
-            bool canBeHorizontal(int x, int y, int size) {
+            bool canBeHorizontal(int x, int y, board b, int size) {
                 // gameMatrix array starting at [0, 0], so we see if > 9 instead of 10
-                return (canBeHorizontalLeft(x, y, size) || canBeHorizontalRight(x, y, size));
+                return (canBeHorizontalLeft(x, y, b, size) || canBeHorizontalRight(x, y, b, size));
             }
 
             /* Can the ship be placed horizontally to the left of the XY coordinate? */
-            bool canBeHorizontalLeft(int x, int y, int size) {
+            bool canBeHorizontalLeft(int x, int y, board b, int size) {
                 for(int i = 0; i < size; i++) {
-                    if(gameMatrix[x - i][y] < 0 || gameMatrix[x - i][y].taken)
+                    if(b.gameMatrix[x - i][y].xAxis < 0 || b.gameMatrix[x - i][y].taken)
                         return false;
                 }
                 return true;
             }
 
             /* Can the ship be placed horizontally to the right of the XY coordinate? */
-            bool canBeHorizontalRight(int x, int y, int size) {
+            bool canBeHorizontalRight(int x, int y, board b, int size) {
                 for(int i = 0; i < size; i++) {
-                    if(gameMatrix[x + i][y] > 9 || gameMatrix[x + i][y].taken)
+                    if(b.gameMatrix[x + i][y].xAxis > 9 || b.gameMatrix[x + i][y].taken)
                         return false;
                 }
                 return true;
             }
 
             /* Can the ship be placed vertically? */
-            bool canBeVertical(int x, int y, int size) {
+            bool canBeVertical(int x, int y, board b, int size) {
                 // gameMatrix array starting at [0, 0], so we see if > 9 instead of 10
-                return (canBeVerticalAbove(x, y, size) || canBeVerticalBelow(x, y, size));
+                return (canBeVerticalAbove(x, y, b, size) || canBeVerticalBelow(x, y, b, size));
             }
 
             /* Can the ship be placed vertically above the XY coordinate? */
-            bool canBeVerticalAbove(int x, int y, int size) {
+            bool canBeVerticalAbove(int x, int y, board b, int size) {
                 for(int i = 0; i < size; i++) {
-                    if(gameMatrix[x][y - i] < 0 || gameMatrix[x][y - i].taken)
-                        return false;
-                }
-            }
-
-            /* Can the ship be placed vertically below the XY coordinate? */
-            bool canBeVerticalBelow(int x, int y, int size) {
-                for(int i = 0; i < size; i++) {
-                    if(gameMatrix[x][y + i] < 9 || gameMatrix[x][y + i].taken)
+                    if(b.gameMatrix[x][y - i].yAxis < 0 || b.gameMatrix[x][y - i].taken)
                         return false;
                 }
                 return true;
             }
 
-            private:
-                map<string, bool> locations; // e.g. 5C, has it been hit?
-                int size;
-                bool sunk;                   // Is this ship sunk?
-        };
-
-    private:
-        boardSlot gameMatrix[10][10];
+            /* Can the ship be placed vertically below the XY coordinate? */
+            bool canBeVerticalBelow(int x, int y, board b, int size) {
+                for(int i = 0; i < size; i++) {
+                    if(b.gameMatrix[x][y + i].yAxis < 9 || b.gameMatrix[x][y + i].taken)
+                        return false;
+                }
+                return true;
+            }
+    };
+    boardSlot gameMatrix[10][10];
 };
--- main.cpp
+++ main.cpp
@@ -9,27 +9,46 @@ int main() {
     board gameBoard;
 
     // Create computers ships
-    auto aircraft = board::ship(5);
-    auto battleship = board::ship(4);
-    auto submarine = board::ship(3);
-    auto destroyer = board::ship(3);
-    auto patrol = board::ship(2);
+    auto aircraft = board::ship("aircraft", 5);
+    auto battleship = board::ship("battleship", 4);
+    auto submarine = board::ship("submarine", 3);
+    auto destroyer = board::ship("destroyer", 3);
+    auto patrol = board::ship("patrol", 2);
 
     // Place computers ships
-    aircraft.placeShip();
-    battleship.placeShip();
-    submarine.placeShip();
-    destroyer.placeShip();
-    patrol.placeShip();
+    aircraft.placeShip(gameBoard);
+    battleship.placeShip(gameBoard);
+    submarine.placeShip(gameBoard);
+    destroyer.placeShip(gameBoard);
+    patrol.placeShip(gameBoard);
+
+    /*for(int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << "Is [" << i << ", " << j << "] taken? " << gameBoard.gameMatrix[i][j].taken << endl;
+        }
+    }*/
+
+    for(int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << gameBoard.gameMatrix[i][j].taken << " ";
+        }
+        cout << endl;
+    }
 
     // Game Loop Start
+    while(userShipsLeft != 0 && computerShipsLeft != 0) {
         // Player X inputs valid attack grid
         // Check to see if attack is a hit
         // Remove/add damage to hit ships
         // Redraw game board
         // Check to see if game is over
-            // If game over - exit loop/end program
+        // If game over - exit loop/end program
         // Toggle next player's turn
+
+        userShipsLeft--;
+        computerShipsLeft--;
+    }
+
     // Game Loop Return
     return 0;
 }
